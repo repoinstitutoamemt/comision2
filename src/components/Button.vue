46194a30d28b71636d2983942dd5b85f4c8f05eb
JoseMiguez98 <josemiguez184@gmail.com>
2021-03-11 20:25:35 -0300

    creado el componente boton

---
+++ src/components/Button.vue
@@ -0,0 +1,17 @@
+<template>
+  <button @click="handleClick">{{ texto }}</button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    texto: String,
+  },
+  methods: {
+    handleClick() {
+      this.$emit('onClick');
+    }
+  }
+}
+</script>
